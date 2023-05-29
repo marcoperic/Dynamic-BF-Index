a4e599de28ccd1fa1c5571ebecde747531e37627
Marco Peric <mperic@knights.ucf.edu>
2023-05-28 20:21:31 -0400

    functional Python bloom filter implementation

--- src/py/filter.py
+++ src/py/filter.py
@@ -7,52 +7,56 @@ import hashlib, math
 class Filter:
 
     def __init__(self, size, prob, num_h = 3) -> None:
-        self.size = 100 # debug
-        self.min_size = size
+        # self.min_size = size
+        self.size = 0
         self.hash_functions = [self.h1, self.h2, self.h3]
         self.data = [0] * size
         self.length = size
         self.prob = float(1 / prob)
 
     def insert(self, element):
-        if type(element) == str:
-            print('str input')
-            element = bytes(element, 'utf-8')
-        else:
-            element = bytes(element)
-
-        hashes = [x(element) for x in self.hash_functions]
+        hashes = [x(self.encode(element)) for x in self.hash_functions]
         for i in hashes: self.data[i] = 1
         
         self.size += 1
         new_prob = self.calculateFP()
-        print('new FP percentage: ' + new_prob)
-
-        if (new_prob > self.prob): self.resize() # chance of FP too high, resize
-
-        return None
+        print('insertion complete - new false positive rate is ' + str(new_prob))
+        # if (new_prob > self.prob): self.resize() # chance of FP too high, resize
     
     def calculateFP(self):
         k = len(self.hash_functions)
-        return pow(1 - math.exp((-1 * k) / (self.length / self.size)), k)
+        return float(pow(1 - math.exp((-1 * k) / (self.length / self.size)), k))
 
     def query(self, element):
-        return None
-    
+        hashes = [x(self.encode(element)) for x in self.hash_functions]
+        for i in hashes:
+            if (self.data[i] == 0):
+                return False
+        
+        return True # probably in the filter
+
     def resize(self):
         return None
     
     def h1(self, x):
         h = hashlib.md5()
         h.update(bytes(x))
-        return int(h.hexdigest(), 16) % self.size
+        return int(h.hexdigest(), 16) % self.length
     
     def h2(self, x):
         h = hashlib.sha1()
         h.update(bytes(x))
-        return int(h.hexdigest(), 16) % self.size
+        return int(h.hexdigest(), 16) % self.length
     
     def h3(self, x):
         h = hashlib.sha224()
         h.update(bytes(x))
-        return int(h.hexdigest(), 16) % self.size
+        return int(h.hexdigest(), 16) % self.length
+    
+    def encode(self, element):
+        if type(element) == str:
+            element = bytes(element, 'utf-8')
+        else:
+            element = bytes(element)
+
+        return element
--- src/py/unittest.py
+++ src/py/unittest.py
@@ -1,5 +1,6 @@
 from filter import Filter
 
 x = Filter(10000, 3)
-# print(x.insert('123'))
-# print(x.calculateFP())
+x.insert('123')
+print(x.query('123'))
+print(x.query('12'))
