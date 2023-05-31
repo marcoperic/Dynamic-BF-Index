c63c671b4f665c30e553957f64af8e45cd8cfdc1
Marco Peric <mperic@knights.ucf.edu>
2023-05-31 18:57:05 -0400

    agnostic implementation for function ptr
    generics and other class structure

--- src/cpp/filter.cpp
+++ src/cpp/filter.cpp
@@ -3,9 +3,41 @@ using namespace std;
 
 class Filter
 {
+    template<typename T>
+    struct HashFunction{typedef void (*type) (T const& element);};
+    template<typename T>
+    void h1(T const& element);
+    template<typename T>
+    void h2(T const& element);
+    template<typename T>
+    void h3(T const& element);
+
+    public:
+    Filter()
+    {
+    }
+
+    private:
+    int size = 0;
+
+    void h1()
+    {
+
+    }
+
+    void h2()
+    {
+
+    }
+
+    void h3()
+    {
+
+    }
 };
 
 int main()
 {
+    Filter f = Filter();
     return 0;
 }
