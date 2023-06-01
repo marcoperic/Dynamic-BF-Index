8a23a2713d400c631728dc2438d54d3fa966c8c7
Marco Peric <mperic@knights.ucf.edu>
2023-05-31 20:13:50 -0400

    hash functions added to header

--- src/cpp/hashfunctions.hpp
+++ src/cpp/hashfunctions.hpp
@@ -5,5 +5,19 @@ template<typename T>
 int h1(T const& element)
 {
     std::cout << element << std::endl;
-    return 0;
+    return 1;
+}
+
+template<typename T>
+int h2(T const& element)
+{
+    std::cout << element << std::endl;
+    return 2;
+}
+
+template<typename T>
+int h3(T const& element)
+{
+    std::cout << element << std::endl;
+    return 3;
 }
