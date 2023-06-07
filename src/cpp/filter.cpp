f0ea7a16be7163afb7b124ad1be9246a94802b9e
Marco Peric <mperic@knights.ucf.edu>
2023-06-07 19:41:02 -0400

    insertion complete and FP rate calculation working

--- src/cpp/filter.cpp
+++ src/cpp/filter.cpp
@@ -5,7 +5,11 @@ using namespace std;
 class Filter
 {
     private:
-    int size = 10000;
+    const size_t length = 10000; // BF not dynamic yet.
+    const int hash_functions = 3;
+    bool *data = new bool(length);
+    size_t size = 110;
+    float prob = 0.0;
 
     public:
     Filter()
@@ -15,7 +19,16 @@ class Filter
     template <typename T>
     bool insert(T const& element)
     {
-        long long hash_value[3] = {h1(element, size), h2(element, size), h3(element, size)};
+        long long hash_value[] = {h1(element, length), h2(element, length), h3(element, length)};        
+        
+        for (long long a: hash_value)
+        {
+            data[a] = true;
+        }
+
+        size++;
+        cout << "insertion complete - new false positive rate is " << calculateFPRate() << endl;
+
         return true;
     }
 
@@ -23,6 +36,12 @@ class Filter
     bool query(T const& element)
     {
         return true;
+    }
+
+    double calculateFPRate()
+    {
+        int k = hash_functions;
+        return pow(1 - exp((-k * (size / (double)length))), k);
     }
 };
 
